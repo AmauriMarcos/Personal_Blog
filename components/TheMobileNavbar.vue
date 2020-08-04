a80078e68296a4262df655310888825ea6dedae8
AmauriMarcos <amaurisantos.m@gmail.com>
2020-08-04 09:31:25 +0200

    add search to mobile

--- components/TheMobileNavbar.vue
+++ components/TheMobileNavbar.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="nav-container">
         <nuxt-link tag='p' to='/' class="nav-container__title"><span class="blooming">Blooming<br></span> Thoughts</nuxt-link>
-        <img class="nav-search" src="../assets/search-blog.svg" alt="">
+
+        <img @click="goHome()" src="../assets/browser.svg" alt="" class="home-icon" :class="{ showHome: homeOpacity}">
+
+        <button class="search" @click="openForm()"></button>
+
+        <div id="search-form">
+            <form @submit.prevent="onSubmit" class="form-search-box" :class="{ open2: formIs }">
+                <input id="search" type="text" placeholder="Search" v-model="text" autocomplete="off">
+                <button type="submit" class="button-search"><img src="../assets/search-blog.svg" alt="" class="icon"></button>       
+            </form>
+        </div>
+
         <nav class="nav">
              
             <div class="menu" @click="changeClass()">
@@ -36,18 +47,44 @@ export default {
     data(){
         return{
             categories: [],      
-            isActive: false
+            isActive: false,
+            formIs: false,
+            homeOpacity: false
         }
     }, 
+    
   async created(){
      const res = await axios.get("https://blooming-thoughts.herokuapp.com/categories")
      this.categories = res.data
   },
   methods: {
       changeClass(){
-          this.isActive = !this.isActive;        
+        this.isActive = !this.isActive;  
+        this.homeOpacity = !this.homeOpacity;   
+        
+        if(this.homeOpacity){
+            this.homeOpacity = true;
+        }
+      },
+      openForm(){
+        this.formIs = !this.formIs;   
+        this.homeOpacity = !this.homeOpacity;    
       },
       removeClass(){
+        this.isActive = false;
+        this.formIs = false;
+        this.homeOpacity = false;
+      },
+      onSubmit(){
+        this.$router.push(`/search/${this.text}`)
+        this.text = '' 
+        this.formIs = false;
+        this.homeOpacity = false;
+      },
+      goHome(){
+          this.$router.push("/");
+          this.formIs = false;
+          this.homeOpacity = false;
           this.isActive = false;
       }
   }
@@ -79,13 +116,20 @@ export default {
         }
     }
 
-    .nav-search{
-        width: 32px;
-        height: 32px;
+    .home-icon{
+        width: 30px;
+        height: 30px;
         position: absolute;
-        top: 2rem;
-        right: 8rem;
+        top: 2.3rem;
+        left: 3.5rem;
+        z-index: 700;
+        opacity: 0;
+        cursor: pointer;
+        outline: none;
+
     }
+
+
     .nav-container{
         height: 6rem;
         width: 100%;
@@ -149,7 +193,120 @@ export default {
 
     }
 
-    ul {
+    form{
+
+        display: flex;       
+        justify-content: center;
+        align-items: center;
+        border-bottom-right-radius: 5px;
+        border-bottom-left-radius: 5px;
+
+        @include respond(tab-port){ 
+            position: fixed;      
+            z-index: 500;
+            background: radial-gradient(#b5eee4, #6bb9ac);
+            height:100vh;      
+            width: 100%;   
+            flex-direction: column;     
+            clip-path: circle(100px at 90% -35%);
+            transition: all .3s ease-out;  
+                  
+        }
+    }
+
+    .button-search{
+        border: none;
+        background-color: transparent;
+        padding: .5rem 1rem;
+        position: absolute;
+        right: 4rem;
+        bottom: 19.8rem;
+         cursor: pointer;
+         z-index:10;
+    }
+
+    .icon{
+        width: 32px;
+        height: 32px;
+        cursor: pointer;
+        
+    }
+
+    #search{
+        z-index: 1000;
+        border: none;
+        border-bottom: 1px solid #ffffff;
+        width: 80%;
+        padding: 1rem .5rem;
+        outline: none;
+        font-family: 'Montserrat', sans-serif; 
+        font-size: 2rem;
+        transform: translateY(2rem);
+        color: #ffffff;
+        position: relative;
+        z-index: 5;
+        
+    }
+
+    ::placeholder{
+        font-family: 'Montserrat', sans-serif; 
+        font-size: 2rem;
+        color: #ffffff;
+        font-weight: 200;
+    }
+
+    .search {
+        position: absolute;
+        margin: auto;
+        z-index: 600;
+        top: 0;
+        right: -10rem;
+        bottom: 0;
+        outline: none;
+        left: 0;
+        width: 40px;
+        height: 40px;
+        background: #EE7B54;
+        border-radius: 50%;
+        transition: all 1s;
+        transform: translateX(.4rem);
+        z-index: 4;
+        box-shadow: 0 0 4px 0 rgba(70, 70, 70, 0.4);
+      // box-shadow: 0 0 25px 0 crimson;
+    &:hover {
+      cursor: pointer;
+    }
+    &::before {
+      content: "";
+      position: absolute;
+      margin: auto;
+      top: 13px;
+      right: 0;
+      bottom: 0;
+      left: 13px;
+      width: 8px;
+      height: 2px;
+      background: white;
+      transform: rotate(45deg);
+      transition: all .5s;
+    }
+    &::after {
+      content: "";
+      position: absolute;
+      margin: auto;
+      top: -5px;
+      right: 0;
+      bottom: 0;
+      left: -2px;
+      width: 15px;
+      height: 15px;
+      border-radius: 50%;
+      border: 2px solid white;
+      transition: all .5s;
+    }
+  }
+
+    ul,  {
         width: 50%;
         height: 100%;
         list-style: none;
@@ -210,6 +367,21 @@ export default {
 
     .open{
         clip-path: circle(1300px at 90% -35%);
+        
+       
+    }
+
+    .open2{
+          clip-path: circle(1300px at 90% -35%);
+         
+    }
+
+    .showHome{
+        opacity: 1;
+    }
+
+    .closeHome{
+        opacity: 0;
     }
 
     .close{
